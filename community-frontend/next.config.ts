55ea89c5835a5acdb72c814dc3ac86714ad1e4e0
MD Abdur Rahman Akash <akash22205101195@diu.edu.bd>
2025-03-12 00:31:32 +0600

    Modify title 0008

--- community-frontend/next.config.ts
+++ community-frontend/next.config.ts
@@ -5,10 +5,6 @@ module.exports = {
   images: {
     domains: ["cdn.pixabay.com"], // ✅ Allow images from Pixabay
   },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-  },
 };
 
 const nextConfig: NextConfig = {
