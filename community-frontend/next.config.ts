b40b84c050fdee08d44872f492a78ddcaf66d173
MD Abdur Rahman Akash <akash22205101195@diu.edu.bd>
2025-03-12 00:03:15 +0600

    Modify next.config.ts 0007

--- community-frontend/next.config.ts
+++ community-frontend/next.config.ts
@@ -1,8 +1,13 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 module.exports = {
   images: {
     domains: ["cdn.pixabay.com"], // ✅ Allow images from Pixabay
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
   },
 };
 
